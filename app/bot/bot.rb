4b01f5518476e4f3ce9cb05944c5a66daea01743
IrfaneTB <youniverse-up@outlook.fr>
2020-03-29 18:21:00 +0400

    Frist commit

---
+++ app/bot/bot.rb
@@ -0,0 +1,10 @@
+
+require 'facebook/messenger'
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(
+  access_token: ACCESS_TOKEN)
+
+Bot.on :message do |message|
+  message.reply(text: 'Hello, human!')
+end
